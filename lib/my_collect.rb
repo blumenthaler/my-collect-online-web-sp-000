fa7ce613b226e2dcc101e2c49cadb3e4fd012cb2
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-22 16:03:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,3 +4,6 @@ def my_collect(collection)
   
 end
 
+# iterate over the collection with a while loop
+# execute code in the block you call it with for each element in the collection (using yield)
+# returns the modified collection
